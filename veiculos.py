8475043704f2c684e135ddde041374742852e9dc
bibiSil <167932856+bibiSil@users.noreply.github.com>
2024-04-23 21:18:25 -0300

    Eu adicionei o projeto da minha ide para dentro do Github

---
+++ veiculos.py
@@ -0,0 +1,200 @@
+class Veiculo:
+    def __init__(self, fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros):
+        self.fabricante = fabricante
+        self.modelo = modelo
+        self.pais = pais
+        self.posicaoX = posicaoX
+        self.posicaoY = posicaoY
+        self.valor = valor
+        self.passa = passageiros
+
+
+def info():
+    print("==> Dados do veiculo")
+    print("Fabricante: " + self.fabricante)
+    print("Modelo: " + self.modelo)
+    print("Pais: " + self.pais)
+    print("PosicaoX: " + self.posicaoX)
+    print("PosicaoY: " + self.posicaoY)
+    print("Valor: " + self.valor)
+    print("Passageiros: " + self.passageiros)
+    print("---\n" * 30)
+
+
+class VeiculoTerrestre:
+    def __init__(self, fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros placa, dono
+
+    ):
+    super().__init__(fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros)
+    self.placa = placa
+    self.dono = dono
+
+
+def info():
+    print("==> Dados do veiculo")
+    print("Fabricante: " + self.fabricante)
+    print("Modelo: " + self.modelo)
+    print("Pais: " + self.pais)
+    print("PosicaoX: " + self.posicaoX)
+    print("PosicaoY: " + self.posicaoY)
+    print("Valor: " + self.valor)
+    print("Passageiros: " + self.passageiros)
+    print("Placa: " + self.placa)
+    print("Dono: " + self.dono)
+    print("---\n" * 30)
+
+
+class Carro:
+    def __init__(self, fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono, combustivel,
+                 classe, potencia):
+        super().__init__(fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono)
+        self.combustivel = combustivel
+        self.classe = classe
+        self.potencia = potencia
+
+    def info():
+        print("==> Dados do veiculo")
+        print("Fabricante: " + self.fabricante)
+        print("Modelo: " + self.modelo)
+        print("Pais: " + self.pais)
+        print("PosicaoX: " + self.posicaoX)
+        print("PosicaoY: " + self.posicaoY)
+        print("Valor: " + self.valor)
+        print("Passageiros: " + self.passageiros)
+        print("Placa: " + self.placa)
+        print("Dono: " + self.dono)
+        print("Combustivel: " + self.combustivel)
+        print("Classe: " + self.classe)
+        print("Potencia: " + self.potencia)
+        print("---\n" * 30)
+
+
+class Caminhao:
+    def __init__(self, fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono, combustivel,
+                 classe, potencia, eixos, peso):
+        super().__init__(fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono, combustivel,
+                         classe, potencia)
+        self.eixos = eixos
+        self.peso = peso
+
+
+def info():
+    print("==> Dados do veiculo")
+    print("Fabricante: " + self.fabricante)
+    print("Modelo: " + self.modelo)
+    print("Pais: " + self.pais)
+    print("PosicaoX: " + self.posicaoX)
+    print("PosicaoY: " + self.posicaoY)
+    print("Valor: " + self.valor)
+    print("Passageiros: " + self.passageiros)
+    print("Placa: " + self.placa)
+    print("Dono: " + self.dono)
+    print("Combustivel: " + self.combustivel)
+    print("Classe: " + self.classe)
+    print("Potencia: " + self.potencia)
+    print("Eixos: " + self.eixos)
+    print("Peso: " + self.peso)
+    print("---\n" * 30)
+
+
+class Moto:
+    def __init__(self, fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono, combustivel,
+                 classe, potencia, cilindradas):
+        super().__init__(fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono, combustivel,
+                         classe, potencia)
+        self.cilindradas = cilindradas
+
+    def info():
+        print("==> Dados do veiculo")
+        print("Fabricante: " + self.fabricante)
+        print("Modelo: " + self.modelo)
+        print("Pais: " + self.pais)
+        print("PosicaoX: " + self.posicaoX)
+        print("PosicaoY: " + self.posicaoY)
+        print("Valor: " + self.valor)
+        print("Passageiros: " + self.passageiros)
+        print("Placa: " + self.placa)
+        print("Dono: " + self.dono)
+        print("Combustivel: " + self.combustivel)
+        print("Classe: " + self.classe)
+        print("Potencia: " + self.potencia)
+        print("Cilindradas: " + self.cilindradas)
+        print("---\n" * 30)
+
+
+class VeiculoAreo:
+    def __init__(self, fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono, combustivel,
+                 classe, potencia, cilindradas, codigo, posicaoZ, empresa):
+        super().__init__(fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono, combustivel,
+                         classe, potencia, cilindradas)
+        self.codigo = codigo
+        self.posisaoZ = posicaoZ
+        self.empresa = empresa
+
+    def info():
+        print("==> Dados do veiculo")
+        print("Fabricante: " + self.fabricante)
+        print("Modelo: " + self.modelo)
+        print("Pais: " + self.pais)
+        print("PosicaoX: " + self.posicaoX)
+        print("PosicaoY: " + self.posicaoY)
+        print("Valor: " + self.valor)
+        print("Passageiros: " + self.passageiros)
+        print("Placa: " + self.placa)
+        print("Dono: " + self.dono)
+        print("Combustivel: " + self.combustivel)
+        print("Classe: " + self.classe)
+        print("Potencia: " + self.potencia)
+        print("Cilindradas: " + self.cilindradas)
+        print("Codigo: " + self.codigo)
+        print("PosicaoZ: " + self.PosicaoZ)
+        print("Empresa: " + self.empresa)
+        print("---\n" * 30)
+
+
+class Helecoptero:
+    def __init__(self, fabricante, modelo, pais, posicaoX, posicaoY, valor, passageiros, placa, dono, combustivel,
+                 classe, potencia, cilindradas, codigo, PosicaoZ, empresa, rotores):
+        self.rotores = rotores
+
+
+def info():
+    print("==> Dados do veiculo")
+    print("Fabricante: " + self.fabricante)
+    print("Modelo: " + self.modelo)
+    print("Pais: " + self.pais)
+    print("PosicaoX: " + self.posicaoX)
+    print("PosicaoY: " + self.posicaoY)
+    print("Valor: " + self.valor)
+    print("Passageiros: " + self.passageiros)
+    print("Placa: " + self.placa)
+    print("Dono: " + self.dono)
+    print("Combustivel: " + self.combustivel)
+    print("Classe: " + self.classe)
+    print("Potencia: " + self.potencia)
+    print("Cilindradas: " + self.cilindradas)
+    print("Codigo: " + self.codigo)
+    print("PosicaoZ: " + self.PosicaoZ)
+    print("Empresa: " + self.empresa)
+    print("Rotores: " + self.rotores)
+    print("---\n" * 30)
+
+
+class Aviao:
+    def __init__(self) -> None:
+        pass
+
+
+class VeiculoMaritimo:
+    def __init__(self) -> None:
+        pass
+
+
+class Navio:
+    def __init__(self) -> None:
+        pass
+
+
+class Submarino:
+    def __init__(self) -> None:
+        pass
